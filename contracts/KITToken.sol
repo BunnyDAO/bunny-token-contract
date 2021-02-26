e8fa31b164bb0314da6f396e47999808f0280ebe
BunnyDAO <78995242+BunnyDAO@users.noreply.github.com>
2021-02-26 13:47:46 -0800

    Update and rename BunnyToken.sol to KITToken.sol

--- contracts/KITToken.sol
+++ contracts/KITToken.sol
@@ -6,7 +6,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract BunnyToken is ERC20, ERC20Pausable, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract KITToken is ERC20, ERC20Pausable, ERC20Detailed, ERC20Mintable, ERC20Burnable {
 
     constructor(
         string memory name,
